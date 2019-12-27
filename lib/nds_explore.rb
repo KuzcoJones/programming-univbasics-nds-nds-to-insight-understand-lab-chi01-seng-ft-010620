d3fb44fecc9d4146a7ee05abee952042e03abdf2
Keenan Jones <keenanjones210@gmail.com>
2019-12-27 03:19:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,13 +13,9 @@ def print_first_directors_movie_titles
   movie_info = stevens_hash[:movies]
   my_hash = directors_database[0][:movies]
   movie_list = 0
-  while movie_list < my_hash.length do
-    puts 
-    movie_list += 1 
-    
-  end
+ 
   # for every hash inside of the directors_database[0][:movies] 
   # return the title 
   
- return my_hash[movie_list][:title]
+ 
 end
